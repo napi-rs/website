f7c8e8fb24dd34f0a8595dcc7287c4086d57dec9
LongYinan <lynweklm@gmail.com>
2020-12-27 02:16:45 +0800

    Add ecosystem and getting started

--- src/docs/introduction/guide-video.jsx
+++ src/docs/introduction/guide-video.jsx
@@ -5,7 +5,7 @@ import GuideVideoURL from './napi-rs-guide.mp4'
 
 export const GuideVideo = () => {
   return (
-    <video controls css={css({ maxWidth: '100%' })}>
+    <video controls css={css({ width: '100%' })}>
       <source src={GuideVideoURL} type="video/mp4" />
       <track kind="captions" srcLang="en" />
     </video>
