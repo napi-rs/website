3fa2b99f37036e32dba7edb0ed8608718858c1a8
LongYinan <lynweklm@gmail.com>
2022-02-10 17:17:41 +0800

    Update nextra, add sponsors

---
+++ components/sponsors.jsx
@@ -0,0 +1,9 @@
+export function Sponsors() {
+  return (
+    <object
+      style={{ ':focusVisible': { outline: 'none' } }}
+      data="https://sponsors.napi.rs/sponsors.svg"
+      type="image/svg+xml"
+    />
+  )
+}
