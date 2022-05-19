418505924944d3770fbfd1f66b2ad6c2889abb2f
LongYinan <lynweklm@gmail.com>
2022-05-19 12:16:03 +0800

    Sponsors svg layout

--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -2,18 +2,16 @@ import { useSSG } from 'nextra/ssg'
 
 export function Sponsors() {
   const sponsorsSVG = useSSG()
-  sponsorsSVG
-    .replace(
-      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800" height="468">',
-      '',
-    )
-    .replace('</svg>', '')
+  const reg =
+    /<svg xmlns="http:\/\/www.w3.org\/2000\/svg" xmlns:xlink="http:\/\/www.w3.org\/1999\/xlink" width="([0-9]+)" height="([0-9]+)">/
+  const [_, width, height] = reg.exec(sponsorsSVG)
+  const svgInner = sponsorsSVG.replace(reg, '').replace('</svg>', '')
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      viewBox="0 0 800 468"
-      dangerouslySetInnerHTML={{ __html: sponsorsSVG }}
+      viewBox={`0 0 ${width} ${height}`}
+      dangerouslySetInnerHTML={{ __html: svgInner }}
     ></svg>
   )
 }
