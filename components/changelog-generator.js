ed3e215dd2de37cdd4e7cc97622afa0e2707bb34
LongYinan <lynweklm@gmail.com>
2023-12-29 12:43:04 +0800

    Fix changelog pages

--- components/changelog-generator.js
+++ components/changelog-generator.js
@@ -1,9 +1,9 @@
-import { useSSG } from 'nextra/ssg'
-import { MDXRemote } from 'next-mdx-remote'
+import { buildDynamicMDX, buildDynamicMeta } from 'nextra/remote'
 
 export const getChangelog = async (packageName, locale = 'en') => {
-  const releases = await fetch(
-    `https://api.github.com/repos/napi-rs/napi-rs/releases?per_page=100`,
+  let page = 1
+  let releases = await fetch(
+    `https://api.github.com/repos/napi-rs/napi-rs/releases?per_page=100&page=${page}`,
     {
       headers: {
         Authorization: `token ${process.env.GITHUB_TOKEN}`,
@@ -11,28 +11,41 @@ export const getChangelog = async (packageName, locale = 'en') => {
     },
   ).then((res) => res.json())
 
+  while (releases.length === 100) {
+    page++
+    releases = releases.concat(
+      await fetch(
+        `https://api.github.com/repos/napi-rs/napi-rs/releases?per_page=100&page=${page}`,
+        {
+          headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`,
+          },
+        },
+      ).then((res) => res.json()),
+    )
+  }
+
   return {
     props: {
-      ssg: releases
-        .filter(({ name }) => name.startsWith(packageName))
-        .map((release) => {
-          const body = release.body
-            .replace(/&#39;/g, "'")
-            .replace(
-              /@([a-zA-Z0-9_-]+)(?=(,| ))/g,
-              '[@$1](https://github.com/$1)',
-            )
-          return `## <a href="${
-            release.html_url
-          }" target="_blank" rel="noopener">${release.tag_name}</a> 
+      ...(await buildDynamicMDX(
+        releases
+          .filter(({ name }) => name.startsWith(packageName))
+          .map((release) => {
+            const body = release.body
+              .replace(/&#39;/g, "'")
+              .replace(
+                /@([a-zA-Z0-9_-]+)(?=(,| ))/g,
+                '[@$1](https://github.com/$1)',
+              )
+            return `## <a href="${
+              release.html_url
+            }" target="_blank" rel="noopener">${release.tag_name}</a> 
   ${new Date(release.published_at).toLocaleDateString(locale)} \n${body}`
-        })
-        .join('\n\n'),
+          })
+          .join('\n\n'),
+      )),
+      ...(await buildDynamicMeta()),
     },
+    revalidate: 10,
   }
-}
-
-export function Changelog() {
-  const releases = useSSG()
-  return <MDXRemote {...releases} />
 }
