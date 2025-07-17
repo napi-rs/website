ff0340b071eab913b33fd4015eb2bfec5becd2ca
LongYinan <lynweklm@gmail.com>
2025-07-17 23:29:02 +0800

    Rename

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,15 +3,16 @@ import { Analytics } from '@vercel/analytics/react'
 import '../style.css'
 import '@/components/landing/style.css'
 
-export default function Nextra({ Component, pageProps }) {
+export default function Nextra(props) {
+  const { Component, pageProps } = props
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
-const layout = getLayout(<Component data-lg-smooth {...pageProps} />)
-return (
-
-<>
-  <Analytics />
-  {layout}
-</>
-) }
+  const layout = getLayout(<Component data-lg-smooth {...pageProps} />)
+  return (
+    <>
+      <Analytics />
+      {layout}
+    </>
+  )
+}
